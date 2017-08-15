8e54095b6fbf411defaa71ad4338e3508a55ecf7
Rhys <rbrooker105@gmail.com>
2017-08-15 18:01:10 +1000

    add createMines function to create grid with randomly placed mines

--- scripts/minesweeper.js
+++ scripts/minesweeper.js
@@ -18,6 +18,7 @@ const difficultyLevels = {
     mines: 99
   }
 }
+let gameArray;
 
 
 const gameGrid = () => {
@@ -31,9 +32,11 @@ const gameGrid = () => {
   return grid;
 };
 
-gameContainer.innerHTML = ''; //Clear container
-//creates grid for the DOM
-gameContainer.insertAdjacentHTML('afterbegin', gameGrid().join(''));
+const renderGrid = () => {
+  const grid = gameGrid().join('')
+  gameContainer.innerHTML = ''; //Clear container
+  gameContainer.insertAdjacentHTML('afterbegin', grid);
+};
 
 const uncover = (target) => {
   //okay, need to uncover square here...
@@ -41,7 +44,39 @@ const uncover = (target) => {
 };
 
 gameContainer.onclick = function(event) {
-  let target = event.target; // where was the click?
-  if (!target.classList.contains('hidden')) return; // not on TD? Then we're not interested
+  let target = event.target; 
+  if (!target.classList.contains('hidden')) return;
   uncover(target);
 };
+
+const randomNumber = (max) => {
+  return Math.floor(Math.random() * (max));
+};
+
+const createMines = (rows, cols) => {
+  let mineGrid = [...Array(rows).keys()].map(i => Array(cols));
+  let totalMines = difficultyLevels[level].mines;
+  let randX;
+  let randY;
+  while (totalMines > 0) {
+    randX = randomNumber(rows);
+    randY = randomNumber(cols);
+    if (mineGrid[randX][randY] === undefined) {
+      mineGrid[randX][randY] = 'mine';
+      totalMines -= 1;
+    }
+  }
+  return mineGrid;
+};
+
+const startGame = () => {
+  renderGrid();
+  const rows = difficultyLevels[level].rows;
+  const columns = difficultyLevels[level].columns;
+  gameArray = createMines(rows, columns);
+
+}
+
+// (global => {
+  startGame();
+// })(window);
